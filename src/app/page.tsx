43b30b656986b8452b35f6191797d5001cb7b1f8
subinkim9755 <82797486+subinkim9755@users.noreply.github.com>
2025-07-04 10:38:47 +0900

    [#6]chore:랜딩페이지에 테스트용 라벨 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,7 @@ import { MoveTypeLabel } from "@/components/common/Chips/MoveTypeLabel";
 import React from "react";
 
 const HomePage = () => {
-  return (
-    <div className="flex flex-col gap-5 px-10 py-10">
-      <MoveTypeLabel type="small" size="sm" />
-      <MoveTypeLabel type="home" size="md" />
-      <MoveTypeLabel type="office" size="responsive" />
-      <MoveTypeLabel type="document" size="responsive" />
-      <CircleTextLabel text="도로명" Clickable={false} />
-      <CircleTextLabel text="상세주소" Clickable={false} />
-      <CircleTextLabel text="부산" Clickable={true} size="sm" />
-      <CircleTextLabel text="소형이사" Clickable={true} size="sm" />
-      <CircleTextLabel text="서울" Clickable={true} size="md" />
-      <CircleTextLabel text="사무실이사" Clickable={true} size="md" />
-      <CircleTextLabel text="가정이사" Clickable={true} size="responsive" />
-      <CircleTextLabel text="서울" Clickable={true} size="responsive" />
-      <div className="flex text-red-500">랜딩 페이지 입니다</div>;
-    </div>
-  );
+  return <div className="flex text-red-500">랜딩 페이지 입니다</div>;
 };
 
 export default HomePage;
