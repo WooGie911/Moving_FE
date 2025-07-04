7ff47e86b77b4b7e6c2886c5273c9a3088ae8ecf
subinkim9755 <82797486+subinkim9755@users.noreply.github.com>
2025-07-04 16:36:06 +0900

    [#6]refactor :Chip컴포넌트 반응형 조절 및 글로벌css 브레이크 포인트 지정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,7 @@ import { MoveTypeLabel } from "@/components/common/Chips/MoveTypeLabel";
 import React from "react";
 
 const HomePage = () => {
-  return (
-    <div className="flex flex-col gap-10 px-20 py-20">
-      <div className="flex flex-col gap-3">
-        <h1>이사유형 라벨</h1>
-        <MoveTypeLabel type="small" />
-        <MoveTypeLabel type="home" />
-        <MoveTypeLabel type="office" />
-        <MoveTypeLabel type="document" />
-      </div>
-      <div className="flex flex-col gap-3">
-        <h1>텍스트라벨</h1>
-        <CircleTextLabel text="도로명" />
-        <CircleTextLabel text="소형이사" clickAble={true} />
-        <CircleTextLabel text="서울" clickAble={true} />
-        <CircleTextLabel text="경기" clickAble={true} />
-      </div>
-      <div className="flex text-red-500">랜딩 페이지 입니다</div>
-    </div>
-  );
+  return <div className="flex text-red-500">랜딩 페이지 입니다</div>;
 };
 
 export default HomePage;
