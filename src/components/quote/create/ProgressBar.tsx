ca760bc80f47a7d963d4f1802df7feb5d9ee9003
y10b <victory.jun01@gmail.com>
2025-07-10 15:02:45 +0900

    fix: 인터페이스 네이밍 수정 및 반응형 추가

--- src/components/quote/create/ProgressBar.tsx
+++ src/components/quote/create/ProgressBar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-interface ProgressBarProps {
+interface IProgressBarProps {
   value: number; // 0~100 (진행률 %)
   label?: string; // (선택) 프로그래스바 위에 표시할 텍스트
 }
 
-const ProgressBar = ({ value, label }: ProgressBarProps) => {
+const ProgressBar = ({ value, label }: IProgressBarProps) => {
   return (
-    <div className="w-full items-center justify-center bg-white p-6">
+    <div className="w-full items-center justify-center bg-white p-6 lg:p-8">
       {label && <div className="text-black-400 mb-4 text-2xl leading-[26px] font-semibold">{label}</div>}
       <div className="h-2 w-full rounded bg-gray-200">
         <div className="bg-primary-400 h-2 rounded" style={{ width: `${value}%` }} />
