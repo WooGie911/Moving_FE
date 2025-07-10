4f0dd8cea20bd964da1bae26281ce38773129ae2
y10b <victory.jun01@gmail.com>
2025-07-10 14:54:47 +0900

    [#80]feat:진행바 제작

---
+++ src/components/quote/create/ProgressBar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ProgressBarProps {
+  value: number; // 0~100 (진행률 %)
+  label?: string; // (선택) 프로그래스바 위에 표시할 텍스트
+}
+
+const ProgressBar = ({ value, label }: ProgressBarProps) => {
+  return (
+    <div className="w-full items-center justify-center bg-white p-6">
+      {label && <div className="text-black-400 mb-4 text-2xl leading-[26px] font-semibold">{label}</div>}
+      <div className="h-2 w-full rounded bg-gray-200">
+        <div className="bg-primary-400 h-2 rounded" style={{ width: `${value}%` }} />
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
