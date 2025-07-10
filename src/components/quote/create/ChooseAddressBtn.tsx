f61d3d13e9ad7b2b9395501962ec6eabcc4ba40f
y10b <victory.jun01@gmail.com>
2025-07-10 14:01:45 +0900

    fix: 컨벤션에 따라 인터페이스 네이밍 수정

--- src/components/quote/create/ChooseAddressBtn.tsx
+++ src/components/quote/create/ChooseAddressBtn.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-interface ChooseAddressBtnProps {
+interface IChooseAddressBtnProps {
   children: React.ReactNode;
   onClick?: () => void;
 }
 
-const ChooseAddressBtn = ({ children, onClick }: ChooseAddressBtnProps) => {
+const ChooseAddressBtn = ({ children, onClick }: IChooseAddressBtnProps) => {
   return (
     <button
       type="button"
       onClick={onClick}
       className={
-        "border-primary-400 text-primary-400 h-[54px] w-full items-center rounded-2xl border bg-white px-6 text-left text-base leading-[26px] font-semibold transition-colors focus:outline-none"
+        "border-primary-400 text-primary-400 h-[54px] w-full items-center rounded-2xl border px-6 text-left text-base leading-[26px] font-semibold transition-colors focus:outline-none"
       }
     >
       {children}
