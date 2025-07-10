adcbdc8e74d58df71c9eb91338fcc8c6e8421c58
sejun <sam8192@naver.com>
2025-07-10 15:55:14 +0900

    feat:기본정보 수정 ui 구현

--- src/pages/moverMyPage/edit/EditPage.tsx
+++ src/pages/moverMyPage/edit/EditPage.tsx
@@ -45,7 +45,6 @@ const EditPage = () => {
     console.log(data);
   };
 
-  // 비밀번호 확인 실시간 체크
   React.useEffect(() => {
     if (watch("newPassword") && watch("newPasswordConfirm")) {
       if (watch("newPassword") !== watch("newPasswordConfirm")) {
@@ -57,111 +56,111 @@ const EditPage = () => {
   }, [watch("newPassword"), watch("newPasswordConfirm")]);
 
   return (
-    <div className="flex min-h-screen w-full justify-center items-center bg-gray-50 px-2 sm:px-4">
-      <div className="w-full max-w-[1200px] px-2 sm:px-6 md:px-10 pt-8 pb-10 bg-white rounded-[32px] flex flex-col justify-center items-end gap-10 sm:gap-16">
-        <div className="self-stretch flex flex-col justify-start items-start gap-6 sm:gap-10">
-          <div className="self-stretch flex flex-col justify-center items-start gap-6 sm:gap-8">
-            <div className="w-44 justify-center text-neutral-800 text-2xl sm:text-3xl font-semibold leading-10">기본정보 수정</div>
-          </div>
-          <div className="self-stretch h-0 outline outline-1 outline-offset-[-0.50px] outline-zinc-100" />
-          <FormProvider {...form}>
-            <form onSubmit={handleSubmit(onSubmit)} className="self-stretch flex flex-col sm:flex-row justify-between items-start gap-8">
-              {/* 왼쪽: 이름/이메일/전화번호 */}
-              <div className="w-full sm:w-[500px] flex flex-col justify-start items-start gap-8">
-                {/* 이름 */}
-                <div className="self-stretch flex flex-col justify-start items-start gap-2 sm:gap-4">
-                  <div className="text-zinc-800 text-lg sm:text-xl font-semibold leading-loose">이름</div>
-                  <TextInput
-                    name="name"
-                    rules={{ required: "이름은 필수입니다." }}
-                    placeholder="이름을 입력해주세요"
-                    inputClassName="h-14 sm:h-16 text-base sm:text-lg text-black"
-                    wrapperClassName="self-stretch"
-                  />
-                </div>
-                <div className="self-stretch h-0 outline outline-1 outline-offset-[-0.50px] outline-zinc-100" />
-                {/* 이메일 */}
-                <div className="self-stretch flex flex-col justify-start items-start gap-2 sm:gap-4">
-                  <div className="text-zinc-800 text-lg sm:text-xl font-semibold leading-loose">이메일</div>
-                  <TextInput
-                    name="email"
-                    inputClassName="h-12 sm:h-14 text-base sm:text-lg text-neutral-400"
-                    wrapperClassName="w-full sm:w-[500px]"
-                    placeholder="이메일을 입력해주세요"
-                    // readOnly는 BaseInput에만 적용되므로, 필요시 TextInput 내부에서 지원하도록 수정 필요
-                  />
-                </div>
-                {/* 전화번호 */}
-                <div className="self-stretch flex flex-col justify-start items-start gap-2 sm:gap-4">
-                  <div className="text-zinc-800 text-lg sm:text-xl font-semibold leading-loose">전화번호</div>
-                  <TextInput
-                    name="phone"
-                    rules={{ required: "전화번호는 필수입니다." }}
-                    placeholder="전화번호를 입력해주세요"
-                    inputClassName="h-14 sm:h-16 text-base sm:text-lg text-black"
-                    wrapperClassName="w-full sm:w-[500px]"
-                  />
-                </div>
+    <div className="min-h-screen w-full bg-white flex items-center justify-center">
+      <div className="w-full max-w-[1200px] px-4 py-10 bg-white rounded-3xl flex flex-col gap-12">
+        <div className="text-2xl font-bold mb-4 text-black !text-black">기본정보 수정</div>
+        <FormProvider {...form}>
+          <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-y-8">
+            {/* 왼쪽: 이름, 이메일, 전화번호 */}
+            <div className="flex flex-col gap-8">
+              {/* 이름 */}
+              <div className="flex flex-col gap-4">
+                <div className="text-zinc-800 text-base lg:text-lg font-semibold leading-relaxed">이름</div>
+                <TextInput
+                  name="name"
+                  rules={{ required: "이름은 필수입니다." }}
+                  placeholder="이름을 입력해주세요"
+                  inputClassName="w-full lg:w-[500px] p-3.5 bg-white rounded-2xl outline outline-[0.5px] outline-offset-[-0.5px] outline-neutral-200 text-base !text-black"
+                  wrapperClassName="w-full lg:w-[500px]"
+                />
               </div>
-              {/* 오른쪽: 비밀번호 변경 */}
-              <div className="w-full sm:w-[500px] flex flex-col justify-start items-start gap-8">
-                {/* 현재 비밀번호 */}
-                <div className="flex flex-col justify-start items-start gap-2 sm:gap-4">
-                  <div className="text-zinc-800 text-lg sm:text-xl font-semibold leading-loose">현재 비밀번호</div>
-                  <PasswordInput
-                    name="currentPassword"
-                    placeholder="현재 비밀번호를 입력해주세요"
-                    inputClassName="h-12 sm:h-14 text-base sm:text-lg text-black"
-                    wrapperClassName="w-full sm:w-[500px]"
-                  />
-                </div>
-                <div className="self-stretch h-0 outline outline-1 outline-offset-[-0.50px] outline-zinc-100" />
-                {/* 새 비밀번호 */}
-                <div className="flex flex-col justify-start items-start gap-2 sm:gap-4">
-                  <div className="text-zinc-800 text-lg sm:text-xl font-semibold leading-loose">새 비밀번호</div>
-                  <PasswordInput
-                    name="newPassword"
-                    placeholder="새 비밀번호를 입력해주세요"
-                    inputClassName="h-12 sm:h-14 text-base sm:text-lg text-black"
-                    wrapperClassName="w-full sm:w-[500px]"
-                  />
-                </div>
-                {/* 새 비밀번호 확인 */}
-                <div className="flex flex-col justify-start items-start gap-2 sm:gap-4">
-                  <div className="text-zinc-800 text-lg sm:text-xl font-semibold leading-loose">새 비밀번호 확인</div>
-                  <PasswordInput
-                    name="newPasswordConfirm"
-                    placeholder="새 비밀번호를 다시 한번 입력해주세요"
-                    inputClassName="h-12 sm:h-14 text-base sm:text-lg text-black"
-                    wrapperClassName="w-full sm:w-[500px]"
-                  />
-                  {errors.newPasswordConfirm?.message && (
-                    <span className="text-state-error text-sm mt-1">{errors.newPasswordConfirm.message}</span>
-                  )}
-                </div>
+              {/* 이메일 */}
+              <div className="flex flex-col gap-4">
+                <div className="text-zinc-800 text-base lg:text-lg font-semibold leading-relaxed">이메일</div>
+                <TextInput
+                  name="email"
+                  inputClassName="w-full lg:w-[500px] p-3.5 bg-white rounded-2xl outline outline-1 outline-offset-[-1px] outline-neutral-200 text-base text-neutral-400 !text-black"
+                  wrapperClassName="w-full lg:w-[500px]"
+                  placeholder="이메일을 입력해주세요"
+                  readOnly
+                />
               </div>
-            </form>
-          </FormProvider>
-        </div>
-        {/* 버튼 영역 */}
-        <div className="w-full sm:w-[500px] flex justify-start items-start gap-3 sm:gap-5">
-          <button
-            type="button"
-            className="flex-1 h-12 sm:h-14 px-4 sm:px-6 py-2 sm:py-4 rounded-2xl shadow-[4px_4px_10px_0px_rgba(195,217,242,0.20)] outline outline-1 outline-offset-[-1px] outline-stone-300 flex justify-center items-center"
-            onClick={() => router.back()}
-          >
-            <div className="w-32 sm:w-72 text-center text-zinc-500 text-base sm:text-lg font-semibold">취소</div>
-          </button>
-          <Button
-            variant="solid"
-            state={isValid ? "default" : "disabled"}
-            className="flex-1 h-12 sm:h-14 p-2 sm:p-4 rounded-2xl text-base sm:text-lg font-semibold"
-            disabled={!isValid}
-          >
-            수정하기
-          </Button>
-        </div>
+              {/* 전화번호 */}
+              <div className="flex flex-col gap-4">
+                <div className="text-zinc-800 text-base lg:text-lg font-semibold leading-relaxed">전화번호</div>
+                <TextInput
+                  name="phone"
+                  rules={{ required: "전화번호는 필수입니다." }}
+                  placeholder="전화번호를 입력해주세요"
+                  inputClassName="w-full lg:w-[500px] p-3.5 bg-white rounded-2xl outline outline-[0.5px] outline-offset-[-0.5px] outline-neutral-200 text-base !text-black"
+                  wrapperClassName="w-full lg:w-[500px]"
+                />
+              </div>
+            </div>
+            {/* 오른쪽: 비밀번호 관련 */}
+            <div className="flex flex-col gap-8">
+              {/* 현재 비밀번호 */}
+              <div className="flex flex-col gap-4">
+                <div className="text-zinc-800 text-base lg:text-lg font-semibold leading-relaxed">현재 비밀번호</div>
+                <PasswordInput
+                  name="currentPassword"
+                  placeholder="현재 비밀번호를 입력해주세요"
+                  inputClassName="w-full lg:w-[500px] p-3.5 bg-white rounded-2xl outline outline-1 outline-offset-[-1px] outline-neutral-200 text-base !text-black placeholder-neutral-400 pr-14"
+                  wrapperClassName="w-full lg:w-[500px] relative"
+                />
+              </div>
+              {/* 새 비밀번호 */}
+              <div className="flex flex-col gap-4">
+                <div className="text-zinc-800 text-base lg:text-lg font-semibold leading-relaxed">새 비밀번호</div>
+                <PasswordInput
+                  name="newPassword"
+                  placeholder="새 비밀번호를 입력해주세요"
+                  inputClassName="w-full lg:w-[500px] p-3.5 bg-white rounded-2xl outline outline-1 outline-offset-[-1px] outline-neutral-200 text-base !text-black placeholder-neutral-400 pr-14"
+                  wrapperClassName="w-full lg:w-[500px] relative"
+                />
+              </div>
+              {/* 새 비밀번호 확인 */}
+              <div className="flex flex-col gap-4">
+                <div className="text-zinc-800 text-base lg:text-lg font-semibold leading-relaxed">새 비밀번호 확인</div>
+                <PasswordInput
+                  name="newPasswordConfirm"
+                  placeholder="새 비밀번호를 다시 한번 입력해주세요"
+                  inputClassName="w-full lg:w-[500px] p-3.5 bg-white rounded-2xl outline outline-1 outline-offset-[-1px] outline-neutral-200 text-base !text-black placeholder-neutral-400 pr-14"
+                  wrapperClassName="w-full lg:w-[500px] relative"
+                />
+                {errors.newPasswordConfirm?.message && (
+                  <span className="text-state-error text-sm mt-1">{errors.newPasswordConfirm.message}</span>
+                )}
+              </div>
+            </div>
+            {/* 버튼 영역: 오른쪽 정렬 */}
+            <div className="col-span-1 lg:col-span-2 flex justify-end gap-4 mt-8">
+              <button
+                type="button"
+                className="h-14 lg:h-[60px] w-full lg:w-[240px] px-6 py-4 rounded-xl shadow-[4px_4px_10px_0px_rgba(195,217,242,0.20)] outline outline-1 outline-offset-[-1px] outline-stone-300 text-neutral-400 text-base font-semibold"
+                onClick={() => router.back()}
+              >
+                취소
+              </button>
+              <Button
+                type="submit"
+                variant="solid"
+                state={isValid ? "default" : "disabled"}
+                className="h-14 lg:h-[60px] w-full lg:w-[240px] p-4 rounded-xl text-white text-base font-semibold bg-[#F9502E] hover:bg-[#e04322]"
+                disabled={!isValid}
+              >
+                수정하기
+              </Button>
+            </div>
+          </form>
+        </FormProvider>
       </div>
+      {/* PasswordInput 아이콘 위치 오버라이드 */}
+      <style jsx global>{`
+        .relative [class*='absolute'][class*='right-'] {
+          right: 1rem !important;
+        }
+      `}</style>
     </div>
   );
 };
