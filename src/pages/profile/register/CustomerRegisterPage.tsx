90853c9d5f4164e25d42566d18f9fe1c0f9a1a2c
gksktl111 <gksktl111@naver.com>
2025-07-17 14:37:38 +0900

    [#109] feat : 기사님 프로필 등록 페이지 UI 구현

--- src/pages/profile/register/CustomerRegisterPage.tsx
+++ src/pages/profile/register/CustomerRegisterPage.tsx
@@ -9,6 +9,12 @@ import { CircleTextLabel } from "@/components/common/chips/CircleTextLabel";
 import { REGION_OPTIONS, SERVICE_OPTIONS } from "@/constant/profile";
 import { Button } from "@/components/common/button/Button";
 
+interface ICustomerRegisterPageProps {
+  profileImage: string;
+  services: string[];
+  regions: string[];
+}
+
 const CustomerRegisterPage = () => {
   const methods = useForm();
   const { watch, handleSubmit } = methods;
@@ -17,15 +23,17 @@ const CustomerRegisterPage = () => {
   const [regions, setRegions] = useState<string[]>(["서울", "경기", "인천"]);
 
   const allFilled = services.length > 0 && regions.length > 0;
-  const onSubmit = (data: any) => {
+  const onSubmit = () => {
     // TODO: 실제 저장 API 연동
     console.log({ ...data, services, regions });
   };
   return (
     <FormProvider {...methods}>
-      <div className="mx-auto flex max-w-[640px] flex-col gap-8 bg-white px-6 py-2 lg:py-4">
+      <div className="mx-auto flex max-w-[327px] flex-col gap-8 bg-white py-4 lg:mt-12 lg:max-w-[640px] lg:px-0 lg:pb-4">
+        {/* 폼 컨테이너 */}
         <form className="flex max-w-[327px] flex-col gap-4 lg:max-w-[640px]" onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-col items-start justify-center self-stretch">
+          {/* 헤더 */}
+          <div className="border-border-light flex flex-col items-start justify-center self-stretch border-b-1 pb-4">
             <span className="text-2lg justify-center leading-relaxed font-bold text-neutral-800 lg:text-3xl">
               프로필 등록
             </span>
@@ -33,16 +41,18 @@ const CustomerRegisterPage = () => {
               추가 정보를 입력하여 회원가입을 완료해주세요.
             </span>
           </div>
-          <div className="outline-border-light mx-auto h-0 w-full outline-1" />
+
           <div className="flex w-full flex-col gap-5 lg:w-[640px]">
-            <div className="flex flex-col gap-4">
+            {/* 프로필 이미지 */}
+            <div className="border-border-light flex flex-col gap-4 border-b-1 pb-4">
               <div className="text-base leading-relaxed font-semibold text-zinc-800">프로필 이미지</div>
               <div className="flex h-[100px] w-[100px] items-center justify-center overflow-hidden rounded-md bg-neutral-100 lg:h-[160px] lg:w-[160px]">
                 <Image src={moverprofileMd} alt="프로필 이미지" width={160} height={160} className="object-cover" />
               </div>
             </div>
-            <div className="outline-border-light mx-auto h-0 w-full outline-1" />
-            <div className="flex flex-col gap-6">
+
+            {/* 이용 서비스 */}
+            <div className="border-border-light flex flex-col gap-6 border-b-1 pb-4">
               <div className="flex flex-col gap-2">
                 <div className="inline-flex items-center gap-1">
                   <span className="text-base leading-relaxed font-semibold text-zinc-800">이용 서비스</span>
@@ -67,7 +77,9 @@ const CustomerRegisterPage = () => {
                 ))}
               </div>
             </div>
-            <div className="outline-border-light mx-auto h-0 w-full outline-1" />
+
+            {/* 내가 사는 지역 */}
+
             <div className="flex flex-col gap-6">
               <div className="flex flex-col gap-2">
                 <div className="inline-flex items-center gap-1">
@@ -100,11 +112,11 @@ const CustomerRegisterPage = () => {
             </div>
           </div>
         </form>
-        <div className="flex w-full flex-col gap-2 lg:flex-row lg:gap-5">
+        <div className="flex w-[327px] flex-col gap-2 lg:w-full lg:flex-row lg:gap-5">
           <Button
             variant="solid"
             width="w-full"
-            height="h-[54px] lg:h-14"
+            height="h-[54px] lg:h-[60px]"
             className="order-1 items-center justify-center rounded-2xl bg-[#F9502E] p-4 text-base leading-relaxed font-semibold text-white lg:order-2"
             onClick={methods.handleSubmit(onSubmit)}
             disabled={!allFilled}
