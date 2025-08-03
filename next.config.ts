a00bb3f1305cd8c6e560e857e8ffafdff95dcddc
jyeon03 <bjlyeon2003@naver.com>
2025-08-03 15:20:44 +0900

    refactor: 랜딩 페이지 이미지 webp로 변경

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   images: {
     domains: ["part4-moving-s3.s3.ap-northeast-2.amazonaws.com", "s3.amazonaws.com", "example.com"],
+    formats: ["image/webp", "image/avif"],
+    deviceSizes: [375, 744, 1200],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30일 캐시
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 
   // TODO : 추후 린트 오류 모두 해결해야함
