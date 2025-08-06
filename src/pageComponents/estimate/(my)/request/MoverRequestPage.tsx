1cf02fa2e2f277c525b50df507bbd7b34c32c06f
subinkim9755 <82797486+subinkim9755@users.noreply.github.com>
2025-08-06 17:47:08 +0900

    chore:기사 - 아래 회색바탕 여백 끝까지 지정

--- src/pageComponents/estimate/(my)/request/MoverRequestPage.tsx
+++ src/pageComponents/estimate/(my)/request/MoverRequestPage.tsx
@@ -43,11 +43,11 @@ export const MoverRequestPage = () => {
     );
   }
   return (
-    <div>
+    <div className="min-h-screen">
       <EstimateRequestAndEstimateTab userType="Mover" />
-      <div className="flex h-full w-full flex-col items-center justify-center gap-7 bg-[#fafafa] px-6 pt-8 pb-6 md:gap-10 md:px-7 md:pb-8 lg:gap-14 lg:px-10 lg:pt-11 lg:pb-9">
+      <div className="flex min-h-screen w-full flex-col items-center justify-start bg-[#fafafa]">
         <div className="flex w-full flex-col items-center justify-center">
-          <div className="mb-[66px] flex w-full max-w-[1200px] flex-col items-center justify-center gap-4 pt-[35px] md:mb-[98px] md:pt-[42px] lg:mb-[122px] lg:grid lg:grid-cols-2 lg:items-start lg:gap-6 lg:pt-[78px]">
+          <div className="mb-[66px] flex w-full max-w-[1200px] flex-col items-center justify-center gap-4 pt-[35px] md:mb-[98px] md:pt-[42px] lg:mb-[122px] lg:grid lg:grid-cols-2 lg:items-start lg:gap-6">
             {data?.map((item, index: number) => (
               <CardList
                 key={item.id}
