5e999495113e4337fc3886257d53be2ae1d8abbd
gksktl111 <gksktl111@naver.com>
2025-08-07 15:44:49 +0900

    refactor : 토글 전환 애니메이션 라이브러리 삭제 후 내부 css 애니메이션으로 대체(번들 최적화)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import { NextConfig } from "next";
+import createBundleAnalyzer from "@next/bundle-analyzer";
 import createNextIntlPlugin from "next-intl/plugin";
+
+const withBundleAnalyzer = createBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   images: {
@@ -22,49 +29,26 @@ const nextConfig: NextConfig = {
     formats: ["image/webp", "image/avif"],
     deviceSizes: [375, 744, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60 * 60 * 24 * 30, // 30일 캐시
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30일
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-
-  // TODO : 추후 린트 오류 모두 해결해야함
   eslint: {
-    ignoreDuringBuilds: true, // 빌드 시 ESLint 무시
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true, // TypeScript 오류도 무시하려면
+    ignoreBuildErrors: true,
   },
 };
 
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
-export default withSentryConfig(withNextIntl(nextConfig), {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+// ⚠️ Plugin wrapping 순서 중요
+const wrappedConfig = withBundleAnalyzer(withNextIntl(nextConfig));
 
+export default withSentryConfig(wrappedConfig, {
   org: "moving-1m",
   project: "javascript-nextjs",
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
 });
